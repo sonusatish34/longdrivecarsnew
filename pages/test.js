01829fa7d1a6e85151f40f5ac5211bd5a05ac3b5
sonusatish34 <sonusatish34@gmail.com>
2024-09-11 23:07:42 +0530

    swde

--- pages/test.js
+++ pages/test.js
@@ -18,7 +18,6 @@ import { GrGroup } from "react-icons/gr";
 import { TbManualGearbox } from "react-icons/tb";
 import { Carousel } from "react-responsive-carousel";
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import disc from './images/cashback.webp'
 const LocationFetcher = () => {
     const [location, setLocation] = useState(null);
     const [error, setError] = useState(null);
@@ -89,7 +88,7 @@ const LocationFetcher = () => {
                 const realdata = result?.data?.results;
 
                 setData(realdata);
-
+                
             } catch (error) {
                 setError(error.message || 'An error occurred while fetching data.');
             } finally {
@@ -124,66 +123,60 @@ const LocationFetcher = () => {
                 </div></div>}
             {error && <p>Error: {error}</p>}
             <div className=' bg-white'>
-                <p className=' text-center pt-5 text-lg font-bold text-black'>Explore Cars Near You in 20 Kms</p>
+                <p className=' text-center py-5 text-lg font-bold'>Explore Cars Near You in 20 Kms</p>
                 <div className='flex flex-col gap-x-8 gap-y-8 lg:flex-wrap lg:flex-row lg:pl-36 overflow-hidden'>
-                    
                     {data?.map((item, index) => (
                         <React.Fragment key={index}>
-                            <div className="bg-white lg:rounded-md  shadow-lg flex flex-col  xs:w-[100%] md:w-72 lg:h-[530px]   h-[630px] lg:hover:scale-105">
-                                <div className="relative lg:h-[530px]  h-[630px] lg:rounded-md">
-                                    <div className="relative z-20 bg-gradient-to-b from-black opacity-90 lg:rounded-md bottom-4">
+                            <div className="bg-white lg:rounded-md  shadow-lg  flex flex-col  xs:w-[100%]  md:w-72 h-[530px] lg:hover:scale-105 ">
+                                <div className="relative h-[530px] lg:rounded-md ">
+                                    <div className=" relative z-20 bg-gradient-to-b from-black opacity-90 lg:rounded-md">
                                         {/* <p className='p-1 font-bold font-manrope text-3xl '>{item?.maker_model}</p> */}
                                         <div className="flex flex-col gap-2 items-end pt-5 pr-5">
-                                            <p className='capitalize p-1 font-bold text-white bg-blue-700 rounded-md  z-50 font-manrope text-base pt-2 '>{item?.maker_model.toLowerCase()}</p>
+                                            <p className='capitalize p-1 font-bold text-white bg-blue-700 rounded-md  z-50 font-manrope text-base px-2'>{item?.maker_model.toLowerCase()}</p>
                                             <p className='flex justify-center items-center p-1 font-bold z-50 text-sm bg-white text-blue-700 rounded-md '> <span></span><span>{Math.round((item?.distance) * 100) / 100} km near you</span></p>
                                         </div>
                                     </div>
                                     <Slider
                                         dots={false}
-                                        infinite={true}
+                                        infinite={false}
                                         speed={500}
                                         slidesToShow={1}
                                         slidesToScroll={1}
-                                        centerMode={true}
-                                        centerPadding={0}
-                                        focusOnSelect={true}
                                         arrows={false}
-                                        // autoplay={true}
-                                        pauseOnFocus={true}
-                                        pauseOnHover={true}
-                                        className="relative bottom-[4.5rem] lg:rounded-md"
+                                        autoplay={false}
+                                        swipe= {true}
+                                        className=" bottom-[5.5rem] lg:rounded-md kkky overflow-hidden"
                                     >
                                         {getOrderedImages(item?.attributes).map((imageSrc, index) => (
                                             <div key={index} onClick={() => {
                                                 // router.push(`/${item.farm_name.toLowerCase().replace(/ /g, "-")}`)
                                             }}>
-                                                <Image className='h-[530px]' width={1000} height={1000} src={replaceText(imageSrc)} alt={`Car image ${index + 1}`} />
+                                                <Image className='h-[500px] rounded-md' width={1000} height={1000} src={replaceText(imageSrc)} alt={`Car image ${index + 1}`} />
                                             </div>
                                         ))}
+
                                     </Slider>
                                     {/* </Link> */}
-                                    <div className="relative z-20 bottom-[12.5rem] lg:bottom-[12.1rem] bg-gradient-to-t from-black opacity-90">
-                                        {/* <p className='p-1 font-bold font-manrope text-3xl '>{item?.maker_model}</p> */}
+                                    <div className="relative z-20 bottom-[13.5rem] bg-gradient-to-t from-black opacity-90 text-white">
                                         <div className="flex gap-2 items-center justify-around pt-5 pr-5 pb-2">
                                             <p className='font-bold text-lg shadow-black'>Book Now</p>
-                                            {/* <span className='p-1 font-bold bg-[#8C52FF] rounded-bl-md  z-50 font-manrope text-sm '>₹{item?.price_24_hours * 24}/day</span> */}
-                                            <p className='capitalize p-1 font-bold text-white bg-blue-700 rounded-md  z-50 font-manrope text-base pt-2 px-2 border-[1px] border-white'>₹ {item?.price_24_hours * 24}/day</p>
-                                            {/* <p className="text-[#556EE6]">₹ {item.price_24_hours} /Day</p> */}
+                                            <p className='capitalize p-1 font-bold text-white bg-blue-700 rounded-md  z-50  text-base pt-2 px-2 border-[1px] border-white'>₹ {item?.price_24_hours * 24}/day</p>
                                         </div>
-                                        <ul className="flex gap-4 justify-center text-sm pt-2 pb-6 font-bold">
+                                        <ul className="flex gap-4 justify-center text-sm pt-2 pb-6 font-bold ">
                                             <li className="border-r-2 border-white flex items-center gap-1 pr-2"><span><BsFillFuelPumpFill className="text-orange-500" /></span><span>{item?.fuel_type}</span></li>
                                             <li className="border-r-2 border-white flex items-center gap-1 pr-2"><span><GrGroup className="text-blue-500" /></span><span>{item?.seater} Seater</span></li>
                                             <li className=" flex items-center gap-1"><span><TbManualGearbox size={20} className="text-red-600" /></span><span>{item?.transmission_type}</span></li>
                                         </ul>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div className="z-20 relative flex justify-between  text-white bottom-[3.25rem] lg:bottom-[2.3rem]">
+                                        <div>
+                                    <div className="z-20 flex justify-between">
                                         <ul className="flex w-full justify-between">
-                                            <li className="bg-green-500 w-full py-3 lg:py-2 text-center lg:rounded-bl-md">
+                                            <li className="bg-green-500 w-full p-2  text-center text-white">
                                                 {" "}
-                                                <Link href="https://api.whatsapp.com/send?phone=+919000478478&text=Hi%0AI%20am%20looking%20for%20a%20car%20booking." target='_blank'>
-                                                    <p className=" flex gap-1 lg:text-sm text-lg justify-center items-center">
+                                                <Link
+                                                    href="https://api.whatsapp.com/send?phone=+919000478478&text=Hi%0AI%20am%20looking%20for%20a%farmhouse%20booking."
+                                                    target="_blank"
+                                                >
+                                                    <p className=" flex gap-1 text-sm justify-center">
                                                         <span>
                                                             <FaWhatsapp size={20} />
                                                         </span>{" "}
@@ -191,11 +184,13 @@ const LocationFetcher = () => {
                                                     </p>
                                                 </Link>
                                             </li>
-                                            <li className="bg-blue-500 w-full py-3 lg:py-2 lg:rounded-br-md">
+                                            <li className="bg-blue-500 w-full p-2  text-white">
+                                                {" "}
                                                 <Link href="tel:9000478478" target="_blank">
-                                                    <p className=" flex gap-1 lg:text-sm text-lg justify-center items-center">
+                                                    <p className=" flex gap-1 text-sm justify-center">
                                                         <span>
-                                                            <BiPhoneCall size={20} />
+                                                            <BiPhoneCall size=
+                                                            {20} />
                                                         </span>{" "}
                                                         <span>Call Us</span>
                                                     </p>
@@ -204,22 +199,25 @@ const LocationFetcher = () => {
                                         </ul>
                                     </div>
                                 </div>
+                                    </div>
+                                </div>
+                                
 
                                 {/* <p className='p-1 font-bold text-red-500 bg-yellow-200 z-50 font-manrope text-3xl lg:pl-20'>{item?.maker_model}</p> */}
                             </div>
-                            {(index + 1 === 3) && (
-                                <div className="bg-[#8d398d] rounded-lg shadow-lg overflow-hidden flex flex-col  xs:w-[90%] justify-center md:w-72 h-[500px]  lg:hover:scale-105">
-                                    <div>
-                                        <Image
-                                            src={disc}
-                                            height={1000}
-                                            width={1000}
-                                            alt='discount'
-                                            className="scale-75"
-                                        />
-                                    </div>
-                                </div>
-                            )}
+                            {/* {(index + 1) % 5 === 0 && (
+                <div className="bg-orange-100 rounded-lg shadow-lg overflow-hidden flex flex-col  xs:w-[90%] justify-center md:w-72 h-[529px] lg:hover:scale-105">
+                    <div>
+                        <Image
+                            src={disc}
+                            height={400}
+                            width={400}
+                            alt='discount'
+                        />
+                    </div>
+                </div>
+
+            )} */}
                         </React.Fragment>
                     ))}
                 </div>
