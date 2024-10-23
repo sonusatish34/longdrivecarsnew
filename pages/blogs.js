16d6f579d94a152fd315b5e0653d8b4813aadd29
sonusatish34 <sonusatish34@gmail.com>
2024-10-23 11:27:57 +0530

    dswd

--- pages/blogs.js
+++ pages/blogs.js
@@ -1,35 +1,9 @@
-import React, { useState } from 'react';
-import CKEditor from 'ckeditor4-react';
+import React from 'react'
 
-export const MyEditor = () => {
-    const [editorData, setEditorData] = useState('');
+function blogs() {
+  return (
+    <div>blogs</div>
+  )
+}
 
-    const handleChange = (event) => {
-        setEditorData(event.editor.getData());
-    };
-
-    const handleSave = () => {
-        console.log('Saved Data: ', editorData);
-        // You can handle the save logic here, e.g., send to an API
-    };
-
-    return (
-        <div>
-            <h2>CKEditor 4 Example</h2>
-            <CKEditor
-                data={editorData}
-                onChange={handleChange}
-                config={{
-                    // You can customize the editor configuration here
-                    toolbar: [
-                        { name: 'basicstyles', items: ['Bold', 'Italic'] },
-                        { name: 'paragraph', items: ['NumberedList', 'BulletedList'] },
-                        { name: 'insert', items: ['Image', 'Table'] },
-                    ],
-                }}
-            />
-            <button onClick={handleSave}>Save</button>
-        </div>
-    );
-};
-
+export default blogs
