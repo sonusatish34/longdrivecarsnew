212f86f67ca6ca872337fc242748c204846ba7bf
sonusatish34 <sonusatish34@gmail.com>
2024-11-01 14:28:18 +0530

    fdfdf

--- pages/index.js
+++ pages/index.js
@@ -85,7 +85,7 @@ export default function Place({ cars, canonicalUrl }) {
     useEffect(() => {
         const timer = setTimeout(() => {
             setShowPopup(true);
-        }, 6000);
+        }, 4000);
 
         return () => clearTimeout(timer); // Clean up timer
     }, []);
