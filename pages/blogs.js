807191e4376502b8191a0724bdea41e0d8f62f02
sonusatish34 <sonusatish34@gmail.com>
2026-03-10 10:56:47 +0530

    blogs

---
+++ pages/blogs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ComponentName = (props) => {
+  return (
+    <div className='bg-white w-screen h-screen'>
+      Blogs
+    </div>
+  );
+};
+
+export default ComponentName;
